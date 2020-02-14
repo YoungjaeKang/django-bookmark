e3c649004ea10a65454c5a1c41e224aa727cdea6
yjk <kyj8028@hotmail.com>
2020-02-14 22:15:57 +0900

    #4.0 Set Virtualenv for Mac

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%4t91y!i774$zl6i5s9xokaiurztzoqo66#ihrhkjtx3crq9b3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 # 리스트 안에 '*'를 추가해줘야 배포했을 때 접근이 가능함
